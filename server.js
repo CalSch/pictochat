1171f172a8738c6924a127083b3469a7cd14707b
CalSch <cal.schwick@gmail.com>
2022-06-13 13:30:17 -0500

    changed server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/pictochat'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(
+	path.join(__dirname+'/dist/pictochat'+req.url));
 });
 
 // Start the app by listening on the default Heroku port
